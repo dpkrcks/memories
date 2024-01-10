13ea9565a9eddf2e66685bf8fe531c14a06c0a29
dpkrcks <111084714+dpkrcks@users.noreply.github.com>
2024-01-10 02:03:51 -0800

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://dpknegircks:dpkrcks1993@cluster0.4r18ujs.mongodb.net/'
+const CONNECTION_URL = 'Put mongo'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
